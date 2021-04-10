782d368f9f2b2d0d8e6e6b884e3e4fe31d2c09e7
Sanskar Jain <sanskar94511@gmail.com>
2021-04-10 14:30:03 +0530

    Display search results filter-wise

--- src/Client/search/searchController.java
+++ src/Client/search/searchController.java
@@ -4,10 +4,7 @@ import com.mongodb.client.FindIterable;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Cursor;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -25,7 +22,7 @@ public class searchController
     public CheckBox caseID,name,station,place;
     public VBox options;
     public ScrollPane optionsPane;
-    public TilePane namePanel,stationPanel,placePanel,casePanel;
+    public TitledPane namePanel,stationPanel,placePanel,casePanel;
     public void onActionBack()
     {
 
@@ -125,6 +122,7 @@ public class searchController
         String text=searchField.getText();
         options.setVisible(false);
         optionsPane.setVisible(false);
+        displayLabel.setVisible(true);
         if(text.equals(""))
         {
             searchField.setText("");
@@ -141,19 +139,171 @@ public class searchController
                 Document findQuery=new Document();
                 findQuery.append("name",regQuery);
                 FindIterable<Document> result= MongoDB.personCollection.find(findQuery);
+                VBox vBox=new VBox();
                 for(Document r:result)
                 {
-                    Pane pane=new Pane();
+                    HBox pane=new HBox();
                     Label lname=new Label();
                     Label Name=new Label();
                     lname.setText("Name:");
-                    Name.setText(r.getString("name"));
+                    Name.setText(r.getString("name")+"\t");
                     Label laadhaar=new Label();
                     Label Aadhaar=new Label();
+                    laadhaar.setText("Aadhaar No.:");
+                    Aadhaar.setText(r.getString("aadhaarId"));
+                    Button btn=new Button();
+                    btn.setText("View Profile");
+                    btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent mouseEvent) {
+                            //Open view profile page in new window
+                        }
+                    });
+                    pane.getChildren().addAll(lname,Name,laadhaar,Aadhaar,btn);
+                    pane.setPadding(new Insets(10,10,10,10));
+                    vBox.getChildren().add(pane);
                 }
+                namePanel.setContent(vBox);
+            }
+            if(caseID.isSelected())
+            {
+                casePanel.setVisible(true);
+                Document regQuery=new Document();
+                regQuery.append("$regex", "^.*" +Pattern.quote(text));
+                regQuery.append("$options", "i");
+                Document findQuery=new Document();
+                findQuery.append("caseID",regQuery);
+                FindIterable<Document> result= MongoDB.casesCollection.find(findQuery);
+                VBox vBox=new VBox();
+                for(Document r:result)
+                {
+                    HBox pane=new HBox();
+                    Label lID=new Label();
+                    Label ID=new Label();
+                    lID.setText("Case ID:");
+                    ID.setText(r.getString("caseID")+"\t");
+                    Label ldate=new Label();
+                    Label Date=new Label();
+                    ldate.setText("Date:");
+                    Date.setText(r.getString("date"));
+                    Button btn=new Button();
+                    btn.setText("View Case");
+                    btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent mouseEvent) {
+                            //Open view case page in new window
+                        }
+                    });
+                    pane.getChildren().addAll(lID,ID,ldate,Date,btn);
+                    pane.setPadding(new Insets(10,10,10,10));
+                    vBox.getChildren().add(pane);
+                }
+                casePanel.setContent(vBox);
+            }
+            if(station.isSelected())
+            {
+                stationPanel.setVisible(true);
+                Document regQuery=new Document();
+                regQuery.append("$regex", "^.*" +Pattern.quote(text));
+                regQuery.append("$options", "i");
+                Document findQuery=new Document();
+                findQuery.append("stationID",regQuery);
+                FindIterable<Document> result= MongoDB.pstationCollection.find(findQuery);
+                VBox vBox=new VBox();
+                for(Document r:result)
+                {
+                    HBox pane=new HBox();
+                    Label lID=new Label();
+                    Label ID=new Label();
+                    lID.setText("Police Station ID:");
+                    ID.setText(r.getString("stationID")+"\t");
+                    Label lcity=new Label();
+                    Label City=new Label();
+                    lcity.setText("City:");
+                    City.setText(r.getString("city"));
+                    pane.getChildren().addAll(lID,ID,lcity,City);
+                    pane.setPadding(new Insets(10,10,10,10));
+                    vBox.getChildren().add(pane);
+                }
+                stationPanel.setContent(vBox);
+            }
+            if(place.isSelected())
+            {
+                placePanel.setVisible(true);
+                Document regQuery=new Document();
+                regQuery.append("$regex", "^.*" +Pattern.quote(text));
+                regQuery.append("$options", "i");
+                Document findQuery=new Document();
+                findQuery.append("city",regQuery);
+                findQuery.append("state",regQuery);
+                FindIterable<Document> result= MongoDB.personCollection.find(findQuery);
+                VBox vBox=new VBox();
+                for(Document r:result)
+                {
+                    HBox pane=new HBox();
+                    Label lname=new Label();
+                    Label Name=new Label();
+                    lname.setText("Person name:");
+                    Name.setText(r.getString("name")+"\t");
+                    Label lcity=new Label();
+                    Label City=new Label();
+                    lcity.setText("City:");
+                    City.setText(r.getString("city"));
+                    Button btn=new Button();
+                    btn.setText("View Person");
+                    btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent mouseEvent) {
+                            //Open view Person page in new window
+                        }
+                    });
+                    pane.getChildren().addAll(lname,Name,lcity,btn);
+                    pane.setPadding(new Insets(10,10,10,10));
+                    vBox.getChildren().add(pane);
+                }
+                result=MongoDB.casesCollection.find(findQuery);
+                for(Document r:result)
+                {
+                    HBox pane=new HBox();
+                    Label lID=new Label();
+                    Label ID=new Label();
+                    lID.setText("Case ID:");
+                    ID.setText(r.getString("caseID")+"\t");
+                    Label lcity=new Label();
+                    Label City=new Label();
+                    lcity.setText("City:");
+                    City.setText(r.getString("city"));
+                    Button btn=new Button();
+                    btn.setText("View Case");
+                    btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent mouseEvent) {
+                            //Open view case page in new window
+                        }
+                    });
+                    pane.getChildren().addAll(lID,ID,lcity,City,btn);
+                    pane.setPadding(new Insets(10,10,10,10));
+                    vBox.getChildren().add(pane);
+                }
+                result=MongoDB.pstationCollection.find(findQuery);
+                for(Document r:result)
+                {
+                    HBox pane=new HBox();
+                    Label lID=new Label();
+                    Label ID=new Label();
+                    lID.setText("Police Station ID:");
+                    ID.setText(r.getString("stationID")+"\t");
+                    Label lcity=new Label();
+                    Label City=new Label();
+                    lcity.setText("City:");
+                    City.setText(r.getString("city"));
+                    pane.getChildren().addAll(lID,ID,lcity,City);
+                    pane.setPadding(new Insets(10,10,10,10));
+                    vBox.getChildren().add(pane);
+                }
+                placePanel.setContent(vBox);
             }
         }
-        displayLabel.setVisible(true);
     }
     public void onActionCancel()
     {
