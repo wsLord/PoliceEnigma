26274e7d298151695eb822806d4b8309d18f5f7e
Sanskar Jain <sanskar94511@gmail.com>
2021-04-10 11:28:34 +0530

    Added realtime search feature with filters

--- src/Client/search/searchController.java
+++ src/Client/search/searchController.java
@@ -1,4 +1,6 @@
 package Client.search;
+import Server.MongoDB;
+import com.mongodb.client.FindIterable;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Cursor;
@@ -8,18 +10,22 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class searchController
 {
     public TextField searchField;
     public Label displayLabel;
-    public CheckBox caseID,name,station,pincode,place;
+    public CheckBox caseID,name,station,place;
     public VBox options;
     public ScrollPane optionsPane;
     public void onActionBack()
@@ -30,6 +36,72 @@ public class searchController
     {
         String text=searchField.getText();
         List<String> results = new ArrayList<>(); //Search for text in database and add it in results array
+        if(name.isSelected())
+        {
+            Document regQuery=new Document();
+            regQuery.append("$regex", "^.*" +Pattern.quote(text));
+            regQuery.append("$options", "i");
+            Document findQuery=new Document();
+            findQuery.append("name",regQuery);
+            FindIterable<Document> result= MongoDB.personCollection.find(findQuery);
+            for(Document r:result)
+            {
+                results.add(r.getString("name"));
+            }
+        }
+        if(caseID.isSelected())
+        {
+            Document regQuery=new Document();
+            regQuery.append("$regex", "^.*" +Pattern.quote(text));
+            regQuery.append("$options", "i");
+            Document findQuery=new Document();
+            findQuery.append("caseID",regQuery);
+            FindIterable<Document> result= MongoDB.casesCollection.find(findQuery);
+            for(Document r:result)
+            {
+                results.add(r.getString("caseID"));
+            }
+        }
+        if(station.isSelected())
+        {
+            Document regQuery=new Document();
+            regQuery.append("$regex", "^.*" +Pattern.quote(text));
+            regQuery.append("$options", "i");
+            Document findQuery=new Document();
+            findQuery.append("stationID",regQuery);
+            FindIterable<Document> result= MongoDB.pstationCollection.find(findQuery);
+            for(Document r:result)
+            {
+                results.add(r.getString("stationID"));
+            }
+        }
+        if (place.isSelected())
+        {
+            Document regQuery=new Document();
+            regQuery.append("$regex", "^.*" +Pattern.quote(text));
+            regQuery.append("$options", "i");
+            Document findQuery=new Document();
+            findQuery.append("city",regQuery);
+            findQuery.append("state",regQuery);
+            FindIterable<Document> result= MongoDB.personCollection.find(findQuery);
+            for(Document r:result)
+            {
+                results.add(r.getString("city"));
+                results.add(r.getString("state"));
+            }
+            result=MongoDB.casesCollection.find(findQuery);
+            for(Document r:result)
+            {
+                results.add(r.getString("city"));
+                results.add(r.getString("state"));
+            }
+            result=MongoDB.pstationCollection.find(findQuery);
+            for(Document r:result)
+            {
+                results.add(r.getString("city"));
+                results.add(r.getString("state"));
+            }
+        }
         options=new VBox();
         options.setVisible(true);
         optionsPane.setVisible(true);
@@ -39,6 +111,8 @@ public class searchController
             Label label=new Label();
             label.setText(r);
             label.setCursor(Cursor.HAND);
+            label.setFont(Font.font(15));
+            label.setTextFill(Color.BLUEVIOLET);
             label.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent mouseEvent) {
